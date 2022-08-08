712f855ec0dc9fb19e66fb32c1e1fdafc3108846
Ruy_Barbosa <100041095+RuyBarbosa22@users.noreply.github.com>
2022-08-08 13:48:52 -0300

    Entendendo o básico
    Aprendendo a declarar variáveis  e a fazer cálculos usando elas

---
+++ Python1.py
@@ -0,0 +1,89 @@
+a = 200
+b = 50
+c = a / b
+c
+#Atribuir valores inteiros as variáveis 
+a = 10
+b = 5
+
+#Vamos fazer operações.
+soma = a+b
+soma
+subtracao = a-b
+subtracao
+
+multiplicacao = a*b
+multiplicacao
+
+divisao = a / b
+divisao
+
+#print ("a=", a)
+
+a = 6.5
+b = 2.5
+
+#Vamos fazer operações mais complexas
+
+soma = a+b
+subtracao = a-b
+divisao = a/b
+multiplicacao = a*b
+
+#print("a =", a)
+#print("b =", b)
+#print("a+b =", soma)
+#print("a-b =", subtracao)
+#print("a*b =", multiplicacao)
+#print("a/b =", divisao)
+
+a = 2
+
+potencia_ao_quadrado = a**2
+potencia_ao_cubo = a**3
+potencia_a_quarta = a**4
+
+#print ("Potencia quadratica", potencia_ao_quadrado)
+#print("Potencia cubica", potencia_ao_cubo)
+#print("Potencia a quarta", potencia_a_quarta)
+
+from math import pow 
+
+c = 4
+d = 5
+
+c_elevado_ao_quadrado = pow (c,2)
+c_elevado_ao_cubo = pow (c,3)
+c_elevado_a_quarta = pow (c,4)
+c_elevado_a_d = pow (c,d)
+
+#print("c_elevado_ao_quadrado =", c_elevado_ao_quadrado)
+#print("c_elevado_ao_cubo =", c_elevado_ao_cubo)
+#print("c_elevado_a_quarta =", c_elevado_a_quarta)
+#print("c_elevado_a_d =", c_elevado_a_d)
+
+x = 512
+
+raiz_quadrada_x = x**0.5
+raiz_cubica_x = x** (1/3)
+raiz_quarta_x = x** (1/4)
+
+#print("raiz quadrada de x =", round(raiz_quadrada_x,2))
+#print("raiz cubica de x =", round(raiz_cubica_x,2))
+#print("raiz quarta de x =", round(raiz_quarta_x,2))
+
+x1 = 1.456
+x2 = 3.678
+x3 = 7.5
+
+print(round(x1,1))
+print(round(x1,2))
+print(round(x1,0))
+
+print(round(x2,1))
+print(round(x2,2))
+print(round(x2,0))
+
+print(round(x3,1))
+print(round(x3,2))
+print(round(x3,0))
