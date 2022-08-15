ff5bf252d178d9977aa4458a200ca1143eb58718
Ruy_Barbosa <100041095+RuyBarbosa22@users.noreply.github.com>
2022-08-15 11:04:03 -0300

    Exercícios de python
    Fazendo exercícios de python baseados em IMC e usando float como formatação

---
+++ imc4.py
@@ -0,0 +1,7 @@
+peso = float (input('Quanto você pesa em Kg? (kg)'))
+altura = float (input('Quanto você mede em altura? (m)'))
+IMC = peso/(altura**2)
+print('O seu IMC é de {:.2f}'.format(IMC))
+
+
+
